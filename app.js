06bae6dec1e83d124ccd61fc82bd13dd732e30d3
YD123456789 <a8111202003@gmail.com>
2023-10-11 17:06:25 +0800

    feat: init

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express")
+const exphbs = require('express-handlebars');
+const methodOverride = require('method-override')
+const routes = require('./routes')
+const app = express()
+const port = process.env.PORT || 3000
+
+app.engine('hbs', exphbs.engine({ extname: '.hbs', defaultLayout: "main" }))
+app.set('view engine', 'hbs')
+app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
+app.use(routes)
+
+app.listen(port, () => {
+  console.info(`Example app listening on port ${port}!`)
+})
+
+module.exports = app
