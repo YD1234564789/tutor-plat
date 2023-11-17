8fbf9c97a3e2d30d3e136ca173b72c74362cf37d
YD123456789 <a8111202003@gmail.com>
2023-11-17 13:21:08 +0800

    modify for cyclic v2

--- middleware/multer.js
+++ middleware/multer.js
@@ -1,6 +1,6 @@
 const multer = require('multer')
 // const upload = multer({ dest: 'temp/' })
-const upload = multer({ dest: 'tmp/' })
+const upload = multer({ dest: '/tmp' })
 
 module.exports = upload
 // 設定預設資料夾為temp
