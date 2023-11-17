019065c2bc57f10faedc1d259b1ad3dce07e82ed
YD123456789 <a8111202003@gmail.com>
2023-11-17 13:53:44 +0800

    add @cyclic change fs

--- helpers/file-helper.js
+++ helpers/file-helper.js
@@ -1,4 +1,4 @@
-const fs = require('fs')
+const fs = require('@cyclic.sh/s3fs/promises')
 const imgur =  require('imgur')
 const IMGUR_CLIENT_ID = process.env.IMGUR_CLIENT_ID
 imgur.setClientId(IMGUR_CLIENT_ID)
@@ -24,6 +24,17 @@ const localFileHandler = file => {
       .catch(err => reject(err))
   })
 }
+// 原版
+// const localFileHandler = file => {
+//   return new Promise((resolve, reject) => {
+//     if (!file) return resolve(null)
+//     const fileName = `upload/${file.originalname}`
+//     return fs.promises.readFile(file.path)
+//       .then(data => fs.promises.writeFile(fileName, data))
+//       .then(() => resolve(`/${fileName}`))
+//       .catch(err => reject(err))
+//   })
+// }
 // 照片不存在就跳出，否則從temp複製到upload下並重命名
 
 
